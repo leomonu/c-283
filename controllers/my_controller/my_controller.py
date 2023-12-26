cbd2f419a825bda43c8e954e79a681263d235d9b
leomonu <62434366+leomonu@users.noreply.github.com>
2023-12-26 20:55:11 +0530

    Add files via upload

---
+++ controllers/my_controller/my_controller.py
@@ -0,0 +1,53 @@
+from controller import Robot 
+from controller import Motor
+from controller import Altimeter
+from controller import LED
+import math
+
+class MyController(Robot):
+    def __init__(self):
+    super(MyController,self).__init__()
+    self.timeStep=32
+    
+    
+    # self.distanceSensor=selfgetDistanceSensor("distance_sensor")
+    # self.distanceSensor.enable(timeStep)
+    # print(self.distanceSensor)    
+    
+    
+    self.altimeter=self.getDevice("altimeter")
+    self.altimeter.enable(self.timeStep)
+    
+    
+    self.left_motor=self.getDevice("Left wheel motor")
+    self.right_motor=self.getDevice("right wheel motor")
+
+    
+    self.left_motor=setPosition(math.inf)
+    self.right_motor=self.setPosition(math.inf)
+    
+    self.direction_switch=False
+
+
+def run(self):
+    while self.step(self.timeStep) !=-1:
+    altitude=self.altimeter.getValue()
+    print(altitude)
+    
+    if(not self.direction_switch):
+        self.left_motor.setVelocity(2.0)
+        self.left_motor.setVelocity(2.0)
+        if(altitude<=0.05):
+            self.direction_switch=True
+    
+    else:
+        self.left_motor.setVelocity(-2.0)
+        self.left_motor.setVelocity(-2.0)
+        if(altitude>=0.25):
+            self.direction_switch=False
+            
+            
+            
+        
+controller=MyController()
+controller.run()
